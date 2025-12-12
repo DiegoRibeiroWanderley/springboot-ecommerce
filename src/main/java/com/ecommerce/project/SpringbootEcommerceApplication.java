639570b10e3e204678789ba92af997a6597e3951
Diego <eliwandejp@gmail.com>
2025-12-12 18:11:17 -0300

    updated .gitignore

--- src/main/java/com/ecommerce/project/SpringbootEcommerceApplication.java
+++ src/main/java/com/ecommerce/project/SpringbootEcommerceApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringbootEcommerceApplication {
 
 	public static void main(String[] args) {
-        
 		SpringApplication.run(SpringbootEcommerceApplication.class, args);
 	}
 
