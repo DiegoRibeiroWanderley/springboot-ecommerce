c26abf995aa474e525ad212b958e12c8ba0b1740
Diego <eliwandejp@gmail.com>
2025-12-22 14:49:36 -0300

    Replacing ModelMapper with MapStruct

---
+++ target/generated-sources/annotations/com/ecommerce/project/mapper/CategoryMapperImpl.java
@@ -0,0 +1,45 @@
+package com.ecommerce.project.mapper;
+
+import com.ecommerce.project.model.Category;
+import com.ecommerce.project.payload.CategoryDTO;
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2025-12-22T14:46:50-0300",
+    comments = "version: 1.6.3, compiler: javac, environment: Java 17.0.17 (Microsoft)"
+)
+@Component
+public class CategoryMapperImpl implements CategoryMapper {
+
+    @Override
+    public CategoryDTO toDTO(Category category) {
+        if ( category == null ) {
+            return null;
+        }
+
+        CategoryDTO categoryDTO = new CategoryDTO();
+
+        categoryDTO.setCategoryId( category.getCategoryId() );
+        categoryDTO.setCategoryName( category.getCategoryName() );
+
+        return categoryDTO;
+    }
+
+    @Override
+    public List<CategoryDTO> toDTOs(List<Category> categories) {
+        if ( categories == null ) {
+            return null;
+        }
+
+        List<CategoryDTO> list = new ArrayList<CategoryDTO>( categories.size() );
+        for ( Category category : categories ) {
+            list.add( toDTO( category ) );
+        }
+
+        return list;
+    }
+}
