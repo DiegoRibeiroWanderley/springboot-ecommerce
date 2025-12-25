56e018394e802891d8c7a35dea7a0164aa64713d
Diego <eliwandejp@gmail.com>
2025-12-25 20:31:34 -0300

    Deleting a product

--- src/main/java/com/ecommerce/project/service/ProductService.java
+++ src/main/java/com/ecommerce/project/service/ProductService.java
@@ -8,8 +8,14 @@ import org.springframework.stereotype.Service;
 public interface ProductService {
 
     ProductResponse getAllProducts();
+
     ProductResponse searchByCategory(Long categoryId);
+
     ProductResponse searchByKeyword(String keyword);
+
     ProductDTO addProduct(Long categoryId, Product product);
+
     ProductDTO updateProduct(Product product, Long productId);
+
+    ProductDTO deleteProduct(Long productId);
 }
