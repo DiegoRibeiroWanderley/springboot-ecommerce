ba4ca6bec337649b77c6b7210e11babfb36a317e
Diego <eliwandejp@gmail.com>
2026-01-14 23:52:10 -0300

    Implement adding a product to the cart

---
+++ src/main/java/com/ecommerce/project/controller/CartController.java
@@ -0,0 +1,23 @@
+package com.ecommerce.project.controller;
+
+import com.ecommerce.project.payload.CartDTO;
+import com.ecommerce.project.service.CartService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
+public class CartController {
+
+    private final CartService cartService;
+
+    @PostMapping("cart/products/{productId}/quantity/{quantity}")
+    public ResponseEntity<CartDTO> addProductToCart(@PathVariable Long productId,
+                                                    @PathVariable Integer quantity){
+        CartDTO cartDTO = cartService.addProductToCart(productId, quantity);
+        return new ResponseEntity<>(cartDTO, HttpStatus.CREATED);
+    }
+}
